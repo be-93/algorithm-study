5ec5329265c2906e6e81ae671e26590e2d334b10
ungseokchoi <cus4703@gmail.com>
2022-03-08 16:30:01 +0900

    Initial commit

---
+++ src/test/java/cus/study/algorithm/AlgorithmStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cus.study.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
